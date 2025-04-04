95e16da52c9aa66d7eb0257472c7f3f39159c78f
Dabdiiusuf <daniel.abdinur@hotmail.com>
2025-04-04 17:15:09 +0200

    Second commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import Axios from "axios";
 import Raindrop from "./Components/Raindrop";
 import Search from "./Components/Search";
 import Forecast from "./Components/Forecast";
+import DateLoc from "./Components/DateLoc";
 
 function App() {
   return (
     <>
       <div className="container">
+        <DateLoc />
         <Search />
         <Forecast />
       </div>
--- src/Components/Forecast.jsx
+++ src/Components/Forecast.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const Forecast = () => {
   return (
     <div className="forecast">
-      <div className="box 1"></div>
-      <div className="box 2"></div>
-      <div className="box 3"></div>
+      <div className="box"></div>
+      <div className="box"></div>
+      <div className="box"></div>
     </div>
   );
 };
